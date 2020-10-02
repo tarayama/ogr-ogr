219756bd91f3376a1b9be703d8bf7beeda391c24
Takuya Matsuura <s1f101801419@iniad.org>
2020-10-02 18:32:16 +0900

    heroku修正

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xcm!a&jjni_+^yow)srd(b@*v05=x$vddx*2d3tu*#795-ytjw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 
 # Application definition
@@ -142,3 +145,8 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'EUADAwC0OMc9Ov4I4lBcCD9j'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+
+if not DEBUG:
+    SECRET_KEY = os.environ['SECRET_KEY']
+    import django_heroku #追加
+    django_heroku.settings(locals()) #追加
